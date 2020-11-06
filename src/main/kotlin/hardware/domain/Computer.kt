ced239647ce72b4e79c3cf5d4f9e401eece70fa5
Brian Henning <henninb@msn.com>
2020-11-05 19:14:01 -0600

    updates

--- src/main/kotlin/hardware/domain/Computer.kt
+++ src/main/kotlin/hardware/domain/Computer.kt
@@ -26,10 +26,109 @@ data class Computer(
         @field:Convert(converter = LowerCaseConverter::class)
         @Column(name = "computer_name", unique = true, nullable = false)
         @JsonProperty
-        var computerName: String
+        var computerName: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "processor", unique = true, nullable = false)
+        @JsonProperty
+        var processor: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "processor_codename", unique = true, nullable = false)
+        @JsonProperty
+        var processorCodename: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "motherboard", unique = true, nullable = false)
+        @JsonProperty
+        var motherboard: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "motherboard_chipset", unique = true, nullable = false)
+        @JsonProperty
+        var motherboardChipset: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "operating_system", unique = true, nullable = false)
+        @JsonProperty
+        var operatingSystem: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "location", unique = true, nullable = false)
+        @JsonProperty
+        var location: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "video_card", unique = true, nullable = false)
+        @JsonProperty
+        var videoCard: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "video_card_codename", unique = true, nullable = false)
+        @JsonProperty
+        var videoCardCodename: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "form_factor", unique = true, nullable = false)
+        @JsonProperty
+        var formFactor: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "optical_drive", unique = true, nullable = false)
+        @JsonProperty
+        var opticalDrive: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "solid_state_drive", unique = true, nullable = false)
+        @JsonProperty
+        var solidStateDrive: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "spinning_drive", unique = true, nullable = false)
+        @JsonProperty
+        var spinningDrive: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "memory_size", unique = true, nullable = false)
+        @JsonProperty
+        var memorySize: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "power_supply_unit", unique = true, nullable = false)
+        @JsonProperty
+        var powerSupplyUnit: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "hdmi_port", unique = true, nullable = false)
+        @JsonProperty
+        var hdmiPort: String,
+
+        @field:Size(min = 1, max = 50)
+        @field:Convert(converter = LowerCaseConverter::class)
+        @Column(name = "notes", unique = true, nullable = false)
+        @JsonProperty
+        var notes: String
+
         ) {
 
-    constructor() : this(0L, "")
+    constructor() : this(0L, "","","","","","",
+            "","", "","","","","","",
+            "","","")
 
     override fun toString(): String = mapper.writeValueAsString(this)
 
